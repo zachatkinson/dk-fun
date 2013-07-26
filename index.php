7ed0a6cde28e159c37891d92e6c310729642eaba
zach <zach@machineandmagic.com>
2013-07-26 11:19:48 -0400

    dkfun.js for holding game engine

--- index.php
+++ index.php
@@ -22,26 +22,6 @@
         </canvas>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript" ></script>
         <script src="./js/bootstrap.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            var canvas = document.getElementById("gameScreen");
-            var ctx = canvas.getContext("2d");
-            ctx.fillStyle = "rgb(0, 0, 255)";
-            ctx.save();
-            ctx.fillRect(50, 50, 100, 100);
-
-            ctx.fillStyle = "rgb(255, 0, 0)";
-            ctx.save();
-            ctx.fillRect(200, 50, 100, 100);
-
-            ctx.restore();
-            ctx.fillRect(350, 50, 100, 100);
-
-            ctx.restore();
-            ctx.fillRect(50, 200, 100, 100);
-
-
-        });
-    </script>
+    <script src="./js/" type="text/javascript"></script>
     </body>
 </html>
---
+++ js/dkfun.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * Date: 2013-07-26
+ * Author: Zach Atkinson
+ * Project: dk-fun
+ * File:
+ * © Zach Atkinson 2013. All Rights Reserved
+ *
+ */
